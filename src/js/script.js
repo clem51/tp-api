2487bc5da30cda7b98cb7bef6b9063e4fc9aac56
Clément GUIDON <clem.guidon@gmail.com>
2020-05-21 00:00:33 +0200

    first commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
-console.log('Hello world');
+console.log("Hello world");
+
+// create new request
+const request = new XMLHttpRequest();
+
+// use request to open the api's url
+request.open(
+  "GET",
+  "https://api.scryfall.com/cards/search?order=rarity&q=set%3Awar"
+);
+
+// response type expected
+request.responseType = "json";
+
+request.send();
+
+fetch("https://api.scryfall.com/cards/search?order=rarity&q=set%3Awar").then(
+  function (response) {
+    response.json().then(function (json) {
+      poemDisplay.textContent = json;
+    });
+  }
+);
