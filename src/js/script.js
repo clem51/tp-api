bc094f0eeff2e30cd1cf3b6cbf5d0e005c24cd95
Clément GUIDON <clem.guidon@gmail.com>
2020-05-22 18:54:05 +0200

    Create Boosters and diplay cards on screen

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,35 @@
-let myList = document.querySelector(".cards");
-
+const API_URL =
+  "https://api.scryfall.com/cards/search?order=rarity&q=set%3Awar";
+// random number function
 const getRandomNumberInRange = (max) => Math.floor(Math.random() * max);
 
+// pick a random card inside array
 const getRandomCards = (cards, number) => {
   return Array.from({ length: number }).map(
     (_) => cards[getRandomNumberInRange(cards.length)]
   );
 };
 
-fetch("https://api.scryfall.com/cards/search?order=rarity&q=set%3Awar")
+const createCardElement = (card) => {
+  let $el = document.createElement("div");
+  // create image element
+  let $img = document.createElement("img");
+  // set src of the element
+  $img.src = card.image_uris.png;
+  // append it to $el
+  $el.appendChild($img);
+  return $el;
+};
+
+const createBooster = (rares, uncommon, common) => {
+  return [
+    ...getRandomCards(rares, 1),
+    ...getRandomCards(uncommon, 3),
+    ...getRandomCards(common, 11),
+  ];
+};
+
+fetch(API_URL)
   .then((res) => res.json())
   .then((payload) => {
     // request the next page as pagination is set to 175 cards
@@ -25,13 +46,19 @@ fetch("https://api.scryfall.com/cards/search?order=rarity&q=set%3Awar")
         const uncommon = cards.filter((card) => card.rarity == "uncommon");
         // get 11 common cards
         const common = cards.filter((card) => card.rarity == "common");
-        // return only one array from the 3 previous ones
-        return [
-          ...getRandomCards(rares, 1),
-          ...getRandomCards(uncommon, 3),
-          ...getRandomCards(common, 11),
-        ];
+        // return 8 boosters
+        return Array.from({ length: 2 }).map((_) =>
+          createBooster(rares, uncommon, common)
+        );
       });
   })
-  .then((booster) => {})
+  .then((boosters) => {
+    console.log(boosters);
+
+    const $fragment = document.createDocumentFragment();
+    boosters[0].forEach((card) =>
+      $fragment.appendChild(createCardElement(card))
+    );
+    document.querySelector(".cards").appendChild($fragment);
+  })
   .catch((err) => console.log(`Boom ${err}`));
