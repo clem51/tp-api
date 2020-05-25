009639d96ab6f3d94f7dfc80358fd0105518ffd7
Clément GUIDON <clem.guidon@gmail.com>
2020-05-25 22:25:57 +0200

    add javascript select

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,7 @@ const $loader = document.querySelector(".lds-ring");
 const $listContainer = document.querySelector(".cardList");
 const $header = document.querySelector(".header--image");
 const $pickNumber = document.querySelector(".pick--number");
-const $extensionSelector = document.querySelector(".extension--selector");
+//const $extensionSelector = document.querySelector(".extension--selector");
 // random number function
 const getRandomNumberInRange = (max) => Math.floor(Math.random() * max);
 // check if the booster is empty
@@ -20,7 +20,7 @@ $button.addEventListener("click", () => {
   $button.classList.add("hidden");
   $loader.classList.remove("hidden");
   $pickNumber.classList.remove("hidden");
-  $extensionSelector.classList.add("hidden");
+  //$extensionSelector.classList.add("hidden");
   startDraft();
 });
 
@@ -44,7 +44,7 @@ const createRecap = (selectedCards) => {
     $cardsContainer.appendChild($fragment);
   });
   $pickNumber.innerHTML = "";
-  $pickNumber.textContent = "Your draft results";
+  $pickNumber.textContent = "Here's your draft picks";
 };
 
 const createCardElement = (card, index, selectedCards, boosters, handler) => {
@@ -133,3 +133,81 @@ const startDraft = () => {
     })
     .catch((err) => console.log(`Boom ${err}`));
 };
+// code to move to the other file
+var x, i, j, selElmnt, a, b, c;
+/* Look for any elements with the class "custom-select": */
+x = document.getElementsByClassName("custom-select");
+for (i = 0; i < x.length; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  /* For each element, create a new DIV that will act as the selected item: */
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /* For each element, create a new DIV that will contain the option list: */
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < selElmnt.length; j++) {
+    /* For each option in the original select element,
+    create a new DIV that will act as an option item: */
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function (e) {
+      /* When an item is clicked, update the original select box,
+        and the selected item: */
+      var y, i, k, s, h;
+      s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+      h = this.parentNode.previousSibling;
+      for (i = 0; i < s.length; i++) {
+        if (s.options[i].innerHTML == this.innerHTML) {
+          s.selectedIndex = i;
+          h.innerHTML = this.innerHTML;
+          y = this.parentNode.getElementsByClassName("same-as-selected");
+          for (k = 0; k < y.length; k++) {
+            y[k].removeAttribute("class");
+          }
+          this.setAttribute("class", "same-as-selected");
+          break;
+        }
+      }
+      h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function (e) {
+    /* When the select box is clicked, close any other select boxes,
+    and open/close the current select box: */
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+
+function closeAllSelect(elmnt) {
+  /* A function that will close all select boxes in the document,
+  except the current select box: */
+  var x,
+    y,
+    i,
+    arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  for (i = 0; i < y.length; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i);
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < x.length; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+
+/* If the user clicks anywhere outside the select box,
+then close all select boxes: */
+document.addEventListener("click", closeAllSelect);
